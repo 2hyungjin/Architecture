3b32b3bcf6d195c1914e34bbdc54c17602ce975e
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-01-10 20:05:02 +0900

    coroutine
    coroutine

--- MVVM/app/src/main/java/com/example/mvvmexam/MainActivity.kt
+++ MVVM/app/src/main/java/com/example/mvvmexam/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mvvmexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.SupervisorJob
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun insert(){
     }
 }
