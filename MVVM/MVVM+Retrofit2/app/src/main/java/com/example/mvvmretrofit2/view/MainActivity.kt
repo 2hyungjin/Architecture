c25b8245bfe6865226adfc3dc2bd921aa30dbd9a
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-01-13 18:13:47 +0900

    MVVM+Retrofit
    MVVM+Retrofit

--- MVVM/MVVM+Retrofit2/app/src/main/java/com/example/mvvmretrofit2/view/MainActivity.kt
+++ MVVM/MVVM+Retrofit2/app/src/main/java/com/example/mvvmretrofit2/view/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.mvvmretrofit2.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmretrofit2.R
 import com.example.mvvmretrofit2.adapter.UserAdapter
 import com.example.mvvmretrofit2.viewmodel.UserViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var userVM: UserViewModel
@@ -12,6 +16,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
     }
-    
+    fun init(){
+        userAdapter= UserAdapter()
+        rv.adapter=userAdapter
+        rv.layoutManager=LinearLayoutManager(this)
+        initVM()
+    }
+    fun initVM(){
+        userVM=ViewModelProvider(this).get(UserViewModel::class.java)
+        userVM.users.observe(this, Observer {
+            userAdapter.setList(it)
+        })
+    }
 }
--- MVVM/MVVM+Retrofit2/app/src/main/java/com/example/mvvmretrofit2/viewmodel/UserViewModel.kt
+++ MVVM/MVVM+Retrofit2/app/src/main/java/com/example/mvvmretrofit2/viewmodel/UserViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.mvvmretrofit2.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,11 +9,13 @@ import com.example.mvvmretrofit2.retrofit2.UserItem
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class UserViewModel(private val repo: UserRepo) : ViewModel() {
+class UserViewModel() : ViewModel() {
+    private val repo: UserRepo=UserRepo()
     val users = MutableLiveData<ArrayList<UserItem>>()
 
     init {
         getUsers()
+        Log.d("TAG","NETWORK")
     }
 
     fun getUsers() {
@@ -20,6 +23,7 @@ class UserViewModel(private val repo: UserRepo) : ViewModel() {
             repo.getUsers().let { res ->
                 if (res.isSuccessful) {
                     users.postValue(res.body())
+                    Log.d("TAG",res.body().toString())
                 }
             }
         }
