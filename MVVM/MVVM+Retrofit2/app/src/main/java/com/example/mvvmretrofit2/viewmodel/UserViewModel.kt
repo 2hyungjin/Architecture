9ee807e784a8913d8768abc78442e3b019f4d4cb
2hyungjin <63038103+2hyungjin@users.noreply.github.com>
2021-01-13 17:07:09 +0900

    VM
    VM

--- MVVM/MVVM+Retrofit2/app/src/main/java/com/example/mvvmretrofit2/viewmodel/UserViewModel.kt
+++ MVVM/MVVM+Retrofit2/app/src/main/java/com/example/mvvmretrofit2/viewmodel/UserViewModel.kt
@@ -1,5 +1,28 @@
 package com.example.mvvmretrofit2.viewmodel
 
-class UserViewModel {
-    
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmretrofit2.repository.UserRepo
+import com.example.mvvmretrofit2.retrofit2.UserItem
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel(private val repo: UserRepo) : ViewModel() {
+    val users = MutableLiveData<ArrayList<UserItem>>()
+
+    init {
+        getUsers()
+    }
+
+    fun getUsers() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repo.getUsers().let { res ->
+                if (res.isSuccessful) {
+                    users.postValue(res.body())
+                }
+            }
+        }
+    }
+
 }
